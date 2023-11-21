05d0985555bd5d593badaf724b0062334612ed92
DESKTOP-9HN9MRC\kkul2 <kkul2000@naver.com>
2023-11-21 15:20:22 +0900

    지도그래프 가중치 설정, 경로 오류 수정

--- app.py
+++ app.py
@@ -12,25 +12,35 @@ def calculate_route():
     start_point = data.get('start_point')
     arrive_point = data.get('arrive_point')
     
-    G = nx.read_gpickle('graph5.pkl')
+    G = nx.read_gpickle('graph7.pkl')
     start_node = ox.nearest_nodes(G, start_point[0], start_point[1])
     arrive_node = ox.nearest_nodes(G, arrive_point[0], arrive_point[1])
     
-    route = nx.astar_path(G, start_node, arrive_node, weight='length')
+    def custom_weight(u, v, data):
+        length_weight = data.get('length')
+        final_weight = data.get('final')
+        return length_weight + final_weight
     
-    route_coords = []
-    for i in range(len(route) - 1): #엣지 linestring에 있는 점들까지 포함하도록
+    route = nx.astar_path(G, start_node, arrive_node, weight=custom_weight)
+    
+    route_coords = [] #linestring에 있는 점들도 포함
+    for i in range(len(route) - 1):
         u, v = route[i], route[i + 1]
+        start_coord = G.nodes[u]['x'], G.nodes[u]['y']
+
         if 'geometry' in G[u][v]:
             line = list(G[u][v]['geometry'].coords)
+            if start_coord != line[0]: #linstring 좌표 순서가 경로와 맞는지 확인
+                line = line[::-1]
             del line[-1]
             for point in line:
                 route_coords.append({'X': point[0], 'Y': point[1]})
         else:
             route_coords.append({'X': G.nodes[u]['x'], 'Y': G.nodes[u]['y']})
+    #마지막노드 따로 추가
     route_coords.append({'X': G.nodes[route[-1]]['x'], 'Y': G.nodes[route[-1]]['y']})
     
     return jsonify({"route": route_coords})
 
 if __name__ == '__main__':
-    app.run(port=8000,debug=True)
+    app.run(port=8000)
